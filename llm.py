8a0a3235a22f6593c3634b097c02cbc3e3c77b28
Youssef <youssef.oudghiri5@hotmail.fr>
2024-07-04 00:42:23 +0200

    Update llm.py

--- llm.py
+++ llm.py
@@ -124,7 +124,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     # We use the "STRICT_PACK" strategy below to ensure all vLLM actors are placed on
     # the same Ray node.
     return VLLMDeployment.options(
-        placement_group_bundles=pg_resources, placement_group_strategy="STRICT_PACK"
+        placement_group_bundles=pg_resources, placement_group_strategy="PACK"
     ).bind(
         engine_args,
         parsed_args.response_role,

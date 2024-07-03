c5c7585c1e51769df12d504aafebd22b8fed00ba
Youssef <youssef.oudghiri5@hotmail.fr>
2024-07-04 01:17:24 +0200

    Update llm.py

--- llm.py
+++ llm.py
@@ -111,7 +111,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     Supported engine arguments: https://docs.vllm.ai/en/latest/models/engine_args.html.
     """  # noqa: E501
     cli_args['model'] = "NousResearch/Meta-Llama-3-8B-Instruct"
-    cli_args['tensor_parallel_size'] = 2
+    cli_args['tensor-parallel-size'] = 2
     parsed_args = parse_vllm_args(cli_args)
     engine_args = AsyncEngineArgs.from_cli_args(parsed_args)
     engine_args.worker_use_ray = True
